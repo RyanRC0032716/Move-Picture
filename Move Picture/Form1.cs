f95f83fb92865186b2e1eea7e2c8c7a7825de860
rc0032716 <rc0032716@CRO-24-01>
2019-05-13 14:57:47 +0100

    added an if and else command to left button so it doesnt go off the screen

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -24,7 +24,14 @@ namespace Move_Picture
 
         private void button2_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+            if(pictureBox1.Location.X - 5 > 0)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+            }
+            else
+            {
+                pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
